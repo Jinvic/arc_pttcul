ef16d29f1f8c34ec9a2515e8b0b3dc4969095133
JInvic <2528976435@qq.com>
2023-06-13 03:45:23 +0800

    GUI开发完成
    GUI开发完成

---
+++ GUI_PySide6/arc_pttcul.py
@@ -0,0 +1,266 @@
+import sys
+import csv
+import requests
+from pyquery import PyQuery as pq
+
+from PySide6.QtWidgets import QApplication, QWidget, QInputDialog, QHeaderView
+from PySide6 import QtGui
+from arc_pttcul_ui import Ui_Form
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.model = QtGui.QStandardItemModel()
+        self.ui.tableView.setModel(self.model)
+        header = self.ui.tableView.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.ResizeToContents)
+
+        self.ui.PB_init.clicked.connect(self.init_func)
+        self.ui.PB_CCT_update.clicked.connect(self.CCT_update_func)
+        self.ui.PB_UDT_list.clicked.connect(self.UDT_list_func)
+        self.ui.PB_UDT_add.clicked.connect(self.UDT_add_func)
+        self.ui.PB_UDT_update.clicked.connect(self.UDT_update_func)
+        self.ui.PB_b30.clicked.connect(self.b30_func)
+        self.ui.PB_r10.clicked.connect(self.r10_func)
+
+        self.CCT = 'chart_constant_table.csv'
+        self.UDT = 'user_data_table.csv'
+
+    def init_func(self):
+        if self.CCT_update_func() == 'SSLError':
+            return
+
+        with open(self.UDT, 'w', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            UDT_head = ['曲名', '难度', '定数', '成绩', '潜力值']
+            writer.writerow(UDT_head)
+
+        self.send_message('初始化完成')
+
+    def CCT_update_func(self):
+        target_url = 'https://wiki.arcaea.cn/%E5%AE%9A%E6%95%B0%E8%AF%A6%E8%A1%A8'  # 定数详表页面
+        try:
+            doc = pq(url=target_url)
+        except requests.exceptions.SSLError as e:
+            self.send_message(['爬取定数表失败。\n',
+                               f'错误类型：requests.exceptions.SSLError\n',
+                               '请关闭代理后重试。'])
+            return 'SSLError'
+        chart_constant_table = doc('tbody')  # 定数表
+
+        with open(self.CCT, 'w', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for item in chart_constant_table('tr').items():
+                writer.writerow(item.text().split('\n'))
+            self.send_message('定数表已更新')
+
+    def UDT_list_func(self):
+        UDT_sort()
+        with open('user_data_table.csv', 'r', encoding='utf-8', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            head = next(reader)
+            rows = [row for row in reader]
+            self.csv_list(head, rows)
+            self.send_message('已列出全部成绩')
+
+    def UDT_add_func(self):
+        # text, ok = QInputDialog.getText(parent, '标题', '标签')
+        key_word, okPressed = QInputDialog.getText(self, "查找曲目", "请输入关键词以查找曲目")
+        if(okPressed == False):
+            return
+        dic, head, res, errlog = csv_search(key_word, self.CCT)
+        if(errlog != ''):
+            self.send_message(errlog)
+            return
+        self.send_message(f'已列出包含“{key_word}”的曲目')
+
+        self.csv_list(head, res)
+        # num, ok = QInputDialog.getInt(parent, '标题', '标签', 0, min, max, step)
+        idx, okPressed = QInputDialog.getInt(
+            self, "选择曲目", "请输入对应曲目的序号", 0, 1, len(res), 1)
+        if(okPressed == False):
+            return
+        row = res[idx-1]
+
+        # items = ['选项1', '选项2', '选项3']
+        # item, ok = QInputDialog.getItem(parent, '标题', '标签', items, editable=False)
+        if(len(row) == 5):
+            difficults = ['PST', 'PRS', 'FTR', 'BYD']
+        else:
+            difficults = ['PST', 'PRS', 'FTR']
+        difficult, okPressed = QInputDialog.getItem(
+            self, '选择难度', '选择对应的难度', difficults, editable=False)
+        if(okPressed == False):
+            return
+
+        score, okPressed = QInputDialog.getInt(
+            self, '输入成绩', '输入你的游玩成绩', 0, 0)
+        if(okPressed == False):
+            return
+
+        name = row[0]
+        cc = row[difficults.index(difficult)+1]
+        ptt = ptt_cul(float(cc), score)
+        # 曲名 难度 定数 成绩 潜力值
+        new_row = [name, difficult, cc, score, ptt]
+
+        with open('user_data_table.csv', 'a', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(new_row)
+        UDT_sort()
+        self.send_message(['已添加新成绩：\n', str(new_row)])
+
+    def UDT_update_func(self):
+        key_word, okPressed = QInputDialog.getText(self, "查找曲目", "请输入关键词以查找曲目")
+        if(okPressed == False):
+            return
+        dic, head, res, errlog = csv_search(key_word, self.UDT)
+        if(errlog != ''):
+            self.send_message(errlog)
+            return
+        self.send_message(f'已列出包含“{key_word}”的曲目')
+
+        self.csv_list(head, res)
+        idx, okPressed = QInputDialog.getInt(
+            self, "选择曲目", "请输入对应曲目的序号", 0, 1, len(res), 1)
+        if(okPressed == False):
+            return
+        row = res[idx-1]
+
+        score, okPressed = QInputDialog.getInt(
+            self, '输入成绩', '输入你的游玩成绩', 0, 0)
+        if(okPressed == False):
+            return
+
+        name = row[0]
+        difficult = row[1]
+        cc = row[2]
+        ptt = ptt_cul(float(cc), score)
+        # 曲名 难度 定数 成绩 潜力值
+        new_row = [name, difficult, cc, score, ptt]
+
+        UDT_row_update(dic[idx], new_row)
+        self.send_message(['已更新成绩：\n', str(new_row)])
+
+    def b30_func(self):
+        head, rows, b30 = b30_cul()
+        self.csv_list(head, rows)
+        self.send_message(['已列出b30\n', f'你当前的b30为：{b30}'])
+
+    def r10_func(self):
+        # num, ok = QInputDialog.getDouble(parent, '标题', '标签', 0.00, min, max, decimals)
+        ptt, okPressed = QInputDialog.getDouble(
+            self, "输入ptt", "请输入你当前的ptt", 0.00, 0, 14, 2)
+        if(okPressed == False):
+            return
+
+        head, rows, b30 = b30_cul()
+        r10 = r10_cul(b30, ptt)
+        self.send_message(f'你当前的r10为：{r10}')
+
+    def send_message(self, msgs):
+        self.ui.output_text.setPlainText(''.join(msgs))
+
+    def csv_list(self, head, rows):
+        self.model.clear()
+        self.model.setHorizontalHeaderLabels(head)
+        for row in rows:
+            items = [QtGui.QStandardItem(field) for field in row]
+            self.model.appendRow(items)
+
+
+def csv_search(st, filename):
+    # 读取csv并查询
+    with open(filename, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        head = next(reader)
+        rows = [row for row in reader]
+        res = []
+        dic = {}  # 存查找结果到原表行数的映射
+        row_no = 0  # 原表行数
+        idx = 1  # 结果编号
+        for row in rows:
+            if(row[0].upper().find(st.upper()) != -1):
+                res.append(row)
+                dic[idx] = row_no
+                idx += 1
+            row_no += 1
+
+        if(len(res) == 0):
+            errlog = '未查找到结果，请重新查找'
+        else:
+            errlog = ''
+
+        return dic, head, res, errlog
+
+
+def UDT_row_update(idx, new_row):
+    with open('user_data_table.csv', 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        head = next(reader)
+        rows = [row for row in reader]
+
+    rows[idx] = new_row
+
+    with open('user_data_table.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(head)
+        writer.writerows(rows)
+
+
+def UDT_sort():
+    with open('user_data_table.csv', 'r', encoding='utf-8', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        head = next(reader)
+        rows = [row for row in reader]
+
+    sorted_rows = sorted(rows, key=lambda x: float(x[4]), reverse=True)
+
+    with open('user_data_table.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(head)
+        writer.writerows(sorted_rows)
+
+
+def ptt_cul(cc, score):
+    if(score > 10000000):
+        return cc+2
+    elif(score >= 9800000):
+        return cc+1+(score-9800000)/200000
+    else:
+        return max(cc+(score-9500000)/300000, 0)
+
+
+def b30_cul():
+    with open('user_data_table.csv', 'r', encoding='utf-8', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        head = next(reader)
+        rows = []
+        cnt = 0
+        sum = 0
+        for row in reader:
+            rows.append(row)
+            sum += float(row[4])
+            cnt += 1
+            if(cnt == 30):
+                break
+        return head, rows, sum/30
+
+
+def r10_cul(b30, ptt):
+    return (ptt*40-b30*30)/10
+
+
+def main():
+    app = QApplication(sys.argv)
+    w = Widget()
+    w.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
--- GUI_PySide6/arc_pttcul_ui.py
+++ GUI_PySide6/arc_pttcul_ui.py
@@ -16,77 +16,56 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGroupBox, QHeaderView, QPlainTextEdit,
-    QPushButton, QSizePolicy, QTableView, QVBoxLayout,
-    QWidget)
+    QPushButton, QSizePolicy, QTableView, QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(799, 572)
+        Form.resize(551, 572)
         self.tableView = QTableView(Form)
         self.tableView.setObjectName(u"tableView")
-        self.tableView.setGeometry(QRect(210, 10, 581, 551))
+        self.tableView.setGeometry(QRect(210, 10, 331, 441))
         self.groupBox = QGroupBox(Form)
         self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(10, 10, 191, 451))
-        self.verticalLayoutWidget = QWidget(self.groupBox)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(9, 19, 171, 421))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setSpacing(6)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.PB_init = QPushButton(self.verticalLayoutWidget)
+        self.groupBox.setGeometry(QRect(10, 10, 191, 441))
+        self.PB_init = QPushButton(self.groupBox)
         self.PB_init.setObjectName(u"PB_init")
+        self.PB_init.setGeometry(QRect(10, 20, 169, 51))
         font = QFont()
         font.setPointSize(12)
         self.PB_init.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_init)
-
-        self.PB_CCT_update = QPushButton(self.verticalLayoutWidget)
+        self.PB_CCT_update = QPushButton(self.groupBox)
         self.PB_CCT_update.setObjectName(u"PB_CCT_update")
+        self.PB_CCT_update.setGeometry(QRect(10, 80, 169, 51))
         self.PB_CCT_update.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_CCT_update)
-
-        self.PB_UDT_list = QPushButton(self.verticalLayoutWidget)
+        self.PB_UDT_list = QPushButton(self.groupBox)
         self.PB_UDT_list.setObjectName(u"PB_UDT_list")
+        self.PB_UDT_list.setGeometry(QRect(10, 140, 169, 51))
         self.PB_UDT_list.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_UDT_list)
-
-        self.PB_UDT_update = QPushButton(self.verticalLayoutWidget)
-        self.PB_UDT_update.setObjectName(u"PB_UDT_update")
-        self.PB_UDT_update.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_UDT_update)
-
-        self.PB_UDT_add = QPushButton(self.verticalLayoutWidget)
+        self.PB_UDT_add = QPushButton(self.groupBox)
         self.PB_UDT_add.setObjectName(u"PB_UDT_add")
+        self.PB_UDT_add.setGeometry(QRect(10, 200, 169, 51))
         self.PB_UDT_add.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_UDT_add)
-
-        self.PB_b30 = QPushButton(self.verticalLayoutWidget)
+        self.PB_b30 = QPushButton(self.groupBox)
         self.PB_b30.setObjectName(u"PB_b30")
+        self.PB_b30.setGeometry(QRect(10, 320, 169, 51))
         self.PB_b30.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_b30)
-
-        self.PB_r10 = QPushButton(self.verticalLayoutWidget)
+        self.PB_r10 = QPushButton(self.groupBox)
         self.PB_r10.setObjectName(u"PB_r10")
+        self.PB_r10.setGeometry(QRect(10, 380, 169, 51))
         self.PB_r10.setFont(font)
-
-        self.verticalLayout.addWidget(self.PB_r10)
-
+        self.PB_UDT_update = QPushButton(self.groupBox)
+        self.PB_UDT_update.setObjectName(u"PB_UDT_update")
+        self.PB_UDT_update.setGeometry(QRect(10, 260, 169, 51))
+        self.PB_UDT_update.setFont(font)
         self.groupBox_2 = QGroupBox(Form)
         self.groupBox_2.setObjectName(u"groupBox_2")
-        self.groupBox_2.setGeometry(QRect(10, 460, 191, 101))
+        self.groupBox_2.setGeometry(QRect(10, 450, 531, 111))
         self.output_text = QPlainTextEdit(self.groupBox_2)
         self.output_text.setObjectName(u"output_text")
-        self.output_text.setGeometry(QRect(10, 20, 171, 71))
+        self.output_text.setGeometry(QRect(10, 20, 511, 81))
+        self.output_text.setFont(font)
 
         self.retranslateUi(Form)
 
@@ -94,7 +73,7 @@ class Ui_Form(object):
     # setupUi
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"arc_pttcul", None))
         self.groupBox.setTitle(QCoreApplication.translate("Form", u"\u64cd\u4f5c", None))
         self.PB_init.setText(QCoreApplication.translate("Form", u"\n"
 "\u521d\u59cb\u5316\n"
@@ -105,9 +84,6 @@ class Ui_Form(object):
         self.PB_UDT_list.setText(QCoreApplication.translate("Form", u"\n"
 "\u5217\u51fa\u7528\u6237\u6210\u7ee9\n"
 "", None))
-        self.PB_UDT_update.setText(QCoreApplication.translate("Form", u"\n"
-"\u66f4\u65b0\u73b0\u6709\u6210\u7ee9\n"
-"", None))
         self.PB_UDT_add.setText(QCoreApplication.translate("Form", u"\n"
 "\u6dfb\u52a0\u65b0\u6210\u7ee9\n"
 "", None))
@@ -117,7 +93,11 @@ class Ui_Form(object):
         self.PB_r10.setText(QCoreApplication.translate("Form", u"\n"
 "\u8ba1\u7b97r10\n"
 "", None))
-        self.groupBox_2.setTitle(QCoreApplication.translate("Form", u"\u8f93\u51fa", None))
-        self.output_text.setPlainText("")
+        self.PB_UDT_update.setText(QCoreApplication.translate("Form", u"\n"
+"\u66f4\u65b0\u73b0\u6709\u6210\u7ee9\n"
+"", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("Form", u"\u4fe1\u606f", None))
+        self.output_text.setPlainText(QCoreApplication.translate("Form", u"\u5982\u679c\u4f60\u662f\u7b2c\u4e00\u6b21\u4f7f\u7528\u6b64\u7a0b\u5e8f\uff0c\u8bf7\u5148\u521d\u59cb\u5316\u3002\n"
+"\u6ce8\u610f\uff1a\u5982\u679c\u4f60\u4e0d\u662f\u7b2c\u4e00\u6b21\u4f7f\u7528\uff0c\u521d\u59cb\u5316\u5c06\u6e05\u9664\u6240\u6709\u6570\u636e\uff01", None))
     # retranslateUi
 
